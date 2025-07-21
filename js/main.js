71e72149e4c6572baf4a6b8eaa372f9db9943a20
Rifat <rifatsyq2@gmail.com>
2025-07-21 19:50:56 +0700

    portofolio

--- js/main.js
+++ js/main.js
@@ -85,26 +85,15 @@
 
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
+			loop: false,
+			autoplay: false,
+			touchDrag: false,
+			mouseDrag: false,
+			pullDrag: false,
+			freeDrag: false,
+			dots: false,
+			nav: false,
+			items: 1
 		});
 	};
 	carousel();
